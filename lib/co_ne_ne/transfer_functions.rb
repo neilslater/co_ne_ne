0f27ef298e8d6723eae29a9a28823781618c56b4
Neil Slater <neil@aframe.com>
2013-11-07 19:14:53 +0000

    Unit test for transfer functions

---
+++ lib/co_ne_ne/transfer_functions.rb
@@ -0,0 +1,57 @@
+module CoNeNe::Transfer
+
+  module Sigmoid
+    def self.function x
+      1.0 / (1.0 + Math.exp(-x))
+    end
+
+    def self.bulk_apply_function narr
+      narr.collect! { |x| 1.0 / (1.0 + Math.exp(-x)) }
+    end
+
+    def self.derivative x
+      derivative_at( function( x ) )
+    end
+
+    def self.derivative_at y
+      y * (1.0 - y)
+    end
+  end
+
+  module TanH
+    def self.function x
+      ( 2.0 / (1.0 + Math.exp(-2*x)) ) - 1.0
+    end
+
+    def self.bulk_apply_function narr
+      narr.collect! { |x| ( 2.0 / (1.0 + Math.exp(-2*x)) ) - 1.0 }
+    end
+
+    def self.derivative x
+      derivative_at( function( x ) )
+    end
+
+    def self.derivative_at y
+      1.0 - y * y
+    end
+  end
+
+  module ReLU
+    def self.function x
+      x > 0.0 ? x : 0.0
+    end
+
+    def self.bulk_apply_function narr
+      narr.collect! { |x| x > 0.0 ? x : 0.0 }
+    end
+
+    def self.derivative x
+      x > 0.0 ? 1.0 : 0.0
+    end
+
+    def self.derivative_at y
+      y > 0.0 ? 1.0 : 0.0
+    end
+  end
+
+end
---
+++ spec/transfer_function_spec.rb
@@ -0,0 +1,71 @@
+require 'helpers'
+
+[ CoNeNe::Transfer::Sigmoid, CoNeNe::Transfer::TanH, CoNeNe::Transfer::ReLU ].each do |transfer|
+describe transfer do
+  let( :x_vals ) { (-100..100).to_a.map { |x| x.to_f/10 } }
+  let( :big_array ) { NArray.sfloat(20,20).random(2.0) - 1.0 }
+  let( :original_array ) { big_array.clone }
+
+  describe "#function" do
+    it "outputs a smallish float value for a smallish input value" do
+      x_vals.each do |x|
+        y = transfer.function(x)
+        y.should be_a Float
+        y.should be_within(20.0).of x
+      end
+    end
+
+    it "ouputs same or larger y for increasing x" do
+      max_so_far = -100.0
+      x_vals.each do |x|
+        y = transfer.function(x)
+        y.should be >= max_so_far
+        max_so_far = y
+      end
+    end
+  end
+
+  describe "#bulk_apply_function" do
+    it "alters the whole input array" do
+      big_array.should be_narray_like original_array
+      transfer.bulk_apply_function( big_array )
+      big_array.should_not be_narray_like original_array
+      20.times do |i|
+        20.times do |j|
+          big_array[i,j].should be_within(1e-6).of transfer.function( original_array[i,j] )
+        end
+      end
+    end
+  end
+
+  def approx_dy_dx t, x
+    # For ReLU, dy_dx not well-defined at origin, but we have chosen 0.0
+    return 0.0 if x == 0.0 && t == CoNeNe::Transfer::ReLU
+
+    # This is numerical approximation of derivative
+    d = 5e-6
+    100000 * ( t.function( x + d ) - t.function( x - d ) )
+  end
+
+  describe "#derivative" do
+    it "should return function slope for given x value" do
+      x_vals.each do |x|
+        dy_dx = transfer.derivative(x)
+        dy_dx.should be_a Float
+        dy_dx.should be_within(1e-5).of approx_dy_dx( transfer, x )
+      end
+    end
+  end
+
+  describe "#derivative_at" do
+    it "should return function slope for given y value" do
+      x_vals.each do |x|
+        y = transfer.function(x)
+        dy_dx = transfer.derivative_at(y)
+        dy_dx.should be_a Float
+        dy_dx.should be_within(1e-5).of approx_dy_dx( transfer, x )
+      end
+    end
+  end
+end
+end
