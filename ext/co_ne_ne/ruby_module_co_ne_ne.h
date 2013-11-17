21d7daf234aac45cfa0cf49ef755dc147df2b7c9
Neil Slater <neil@aframe.com>
2013-11-17 19:45:36 +0000

    Start splitting code up into logical layers

---
+++ ext/co_ne_ne/ruby_module_co_ne_ne.h
@@ -0,0 +1,22 @@
+// ext/co_ne_ne/ruby_module_co_ne_ne.h
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Declarations of narray helper functions
+//
+
+#ifndef RUBY_MODULE_CO_NE_NE_H
+#define RUBY_MODULE_CO_NE_NE_H
+
+#include <ruby.h>
+#include "narray.h"
+#include "narray_shared.h"
+#include "convolve_raw.h"
+#include "cnn_components.h"
+#include "transfer_module.h"
+#include "mlp_classes.h"
+#include "mt.h"
+
+void init_module_co_ne_ne();
+
+#endif
