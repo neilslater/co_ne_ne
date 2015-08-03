da1187e3fe704095e7bcbb7afba2ca54323b3ba1
Neil Slater <slobo777@gmail.com>
2015-08-03 21:32:00 +0100

    Update TODO

--- spec/trainer_bp_layer_spec.rb
+++ spec/trainer_bp_layer_spec.rb
@@ -12,6 +12,13 @@ describe RuNeNe::Trainer::BPLayer do
         expect { RuNeNe::Trainer::BPLayer.new( 3, -1 ) }.to raise_error ArgumentError
         expect { RuNeNe::Trainer::BPLayer.new( "hello", 2 ) }.to raise_error TypeError
       end
+
+      it "creates expected sizes and defaults for arrays" do
+        bpl = RuNeNe::Trainer::BPLayer.new( 2, 1 )
+        expect( bpl.de_dz ).to be_narray_like NArray[ 0.0 ]
+        expect( bpl.de_da ).to be_narray_like NArray[ 0.0, 0.0 ]
+
+      end
     end
   end
 end
