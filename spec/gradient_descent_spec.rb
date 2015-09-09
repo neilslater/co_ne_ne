75742a11eca619355fbd9bb23d15d55a916db2fe
Neil Slater <slobo777@gmail.com>
2015-09-09 21:32:09 +0100

    Show that NAG and RMSProp can find minima of Rosenbrock function (sort of)

--- spec/gradient_descent_spec.rb
+++ spec/gradient_descent_spec.rb
@@ -17,6 +17,24 @@ class TestQuadratic
   end
 end
 
+# The Rosenbrock function is very hard for SGD to optimise, so it is a good test of
+# robustness in gradient descent modifiers.
+class TestRosenbrock
+  def value_at params
+    x = params[0]
+    y = params[1]
+    (1.0 - x)**2 + 100.0 * (y - x * x) ** 2
+  end
+
+  def gradients_at params
+    x = params[0]
+    y = params[1]
+    df_dx = -2.0 + 2.0 * x + 400.0 * (x**3 - y * x)
+    df_dy = 200.0 * (y - x**2)
+    NArray.cast( [df_dx,df_dy], 'sfloat' )
+  end
+end
+
 describe RuNeNe::GradientDescent::SGD do
   describe "class methods" do
     let(:example_params) { NArray.sfloat(2) }
@@ -238,95 +256,23 @@ describe RuNeNe::GradientDescent::NAG do
         expect( tq.value_at( @params ) ).to be_within(1e-6).of 0
         expect( @params ).to be_narray_like tq.roots
       end
-    end
-  end
-end
 
-
-describe RuNeNe::GradientDescent::SGD do
-  describe "class methods" do
-    let(:example_params) { NArray.sfloat(2) }
-
-    describe "#new" do
-      it "creates a new object" do
-        expect( RuNeNe::GradientDescent::SGD.new( example_params ) ).to be_a RuNeNe::GradientDescent::SGD
-      end
-
-      it "should take number of params from example input" do
-        gd = RuNeNe::GradientDescent::SGD.new( example_params )
-        expect( gd.num_params ).to be 2
-      end
-
-      it "should take number of params regardless of rank" do
-        gd = RuNeNe::GradientDescent::SGD.new( NArray.cast( [ [ [-1.0, -1.0], [1.0, -1.0] ],
-            [ [-1.0, 1.0], [1.0, 1.0] ], [ [ 1.0, -1.0], [1.0, -1.0] ],
-            [ [ 1.0, -1.0], [1.0, -1.0] ] ], 'sfloat' ) )
-        expect( gd.num_params ).to be 16
-      end
-    end
-
-    describe "with Marshal" do
-      before do
-        @orig_data = RuNeNe::GradientDescent::SGD.new( example_params )
-        @saved_data = Marshal.dump( @orig_data )
-        @copy_data =  Marshal.load( @saved_data )
-      end
-
-      it "can save and retrieve gradient descent settings" do
-        expect( @copy_data ).to_not be @orig_data
-        expect( @copy_data.num_params ).to be 2
-      end
-    end
-  end
-
-  describe "instance methods" do
-    let :gd do
-       RuNeNe::GradientDescent::SGD.new( NArray.sfloat(2) )
-    end
-
-    before :each do
-      NArray.srand(555)
-      @params = (NArray.sfloat(2).random - 0.5) * 10
-    end
-
-    describe "#clone" do
-      it "should make a simple copy of number of params" do
-        copy = gd.clone
-        expect( copy.num_params ).to eql gd.num_params
-      end
-    end
-
-    describe "#pre_gradient_step" do
-      it "should not alter params" do
-        before_params = @params.clone
-        gd.pre_gradient_step( @params, 1.0 )
-        expect( @params ).to be_narray_like( before_params, 1e-16 )
-      end
-    end
-
-    describe "#gradient_step" do
-      it "should alter params" do
-        before_params = @params.clone
-        gradients = NArray.sfloat(2).random - 0.5
-        gd.gradient_step( @params, gradients, 0.1)
-        expect( @params ).to_not be_narray_like before_params
-      end
-    end
-
-    describe "optimisation" do
-      it "should optimise a simple quadratic" do
-        tq = TestQuadratic.new(2)
+      it "should optimise rosenbrock with a bit of help" do
+        tq = TestRosenbrock.new
+        @params = NArray.cast( [ -0.5, 0.5 ], 'sfloat' )
+        gd.momentum = 0.9
 
         # Pre-optimisation check that there is something to optimise
         expect( tq.value_at( @params ) ).to_not be_within(0.1).of 0
-        expect( @params ).to_not be_narray_like tq.roots
+        expect( @params ).to_not be_narray_like NArray[1.0,1.0]
 
-        20.times do
-          gd.pre_gradient_step( @params, 0.1 )
-          gd.gradient_step( @params, tq.gradients_at(@params), 0.1 )
+        7000.times do |t|
+          lr = 0.0005 * 0.9999**t
+          gd.pre_gradient_step( @params, lr )
+          gd.gradient_step( @params, tq.gradients_at(@params), lr )
         end
         expect( tq.value_at( @params ) ).to be_within(1e-6).of 0
-        expect( @params ).to be_narray_like tq.roots
+        expect( @params ).to be_narray_like NArray[1.0,1.0]
       end
     end
   end
@@ -402,7 +348,7 @@ describe RuNeNe::GradientDescent::RMSProp do
 
   describe "instance methods" do
     let :gd do
-       RuNeNe::GradientDescent::RMSProp.new( NArray.sfloat(2), 0.9, 1e-6 )
+       RuNeNe::GradientDescent::RMSProp.new( NArray.sfloat(2), 0.9, 1e-8 )
     end
 
     before :each do
@@ -456,6 +402,23 @@ describe RuNeNe::GradientDescent::RMSProp do
         end
         expect( tq.value_at( @params ) ).to be_within(1e-6).of 0
         expect( @params ).to be_narray_like tq.roots
+      end
+
+      it "should optimise rosenbrock with a bit of help" do
+        tq = TestRosenbrock.new
+        @params = NArray.cast( [ -0.5, 0.5 ], 'sfloat' )
+
+        # Pre-optimisation check that there is something to optimise
+        expect( tq.value_at( @params ) ).to_not be_within(0.1).of 0
+        expect( @params ).to_not be_narray_like NArray[1.0,1.0]
+
+        9000.times do |t|
+          lr = 0.02 * 0.999**t
+          gd.pre_gradient_step( @params, lr )
+          gd.gradient_step( @params, tq.gradients_at(@params), lr )
+        end
+        expect( tq.value_at( @params ) ).to be_within(1e-6).of 0
+        expect( @params ).to be_narray_like NArray[1.0,1.0]
       end
     end
   end
